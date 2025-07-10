164a2491440770a6c09d995d78ea0a8d9b5e7fe8
Md Shamshad Ansari <ershamshad12@gmail.com>
2025-07-10 12:49:59 +0530

    Added new components which is CommentsForm.jsx

---
+++ react_lectures/src/components/CommentsForm.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+export default function CommentsForm() {
+  const [formData, setFormData] = useState({
+    username: "",
+    remarks: "",
+    rating: 5,
+  });
+
+  const handleInputChange = (event) => {
+    let fieldname = event.target.name;
+    let newValue = event.target.value;
+
+    setFormData((currData) => {
+      return { ...currData, [fieldname]: newValue };
+    });
+  };
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    setFormData({
+      username: "",
+      remarks: "",
+      rating: 5,
+    });
+    console.log(formData);
+  };
+
+  return (
+    <div>
+      <h4>Give a comment!</h4>
+      <form onSubmit={formSubmit}>
+        <label htmlFor="username">Username </label>
+        <input
+          type="text"
+          id="username"
+          placeholder="username"
+          value={formData.username}
+          onChange={handleInputChange}
+          name="username"
+        />
+        <br />
+        <br />
+        <label htmlFor="remarks">Remarks </label>
+        <textarea
+          placeholder="Add few remarks"
+          value={formData.remarks}
+          onChange={handleInputChange}
+          id="remarks"
+          name="remarks"
+        ></textarea>
+        <br />
+        <br />
+        <label htmlFor="rating">Rating </label>
+        <input
+          type="number"
+          id="rating"
+          placeholder="rating"
+          min={1}
+          max={5}
+          value={formData.rating}
+          onChange={handleInputChange}
+          name="rating"
+        />
+        <br />
+        <br />
+        <button>Add Comment</button>
+      </form>
+    </div>
+  );
+}
