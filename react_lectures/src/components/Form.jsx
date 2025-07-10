10bcb086b25d45c3fac428b75b3d78c3ab481a93
Md Shamshad Ansari <ershamshad12@gmail.com>
2025-07-10 12:59:26 +0530

    Added input field and states in Form component

--- react_lectures/src/components/Form.jsx
+++ react_lectures/src/components/Form.jsx
@@ -1,22 +1,65 @@
 import { useState } from "react";
 
 export default function Form() {
-  const [fullName, setFullName] = useState("Shamshad");
+  const [formData, setFormData] = useState({
+    fullName: "",
+    username: "",
+  });
 
-  const handleChangeName = (event) => {
-    console.log(event.target.value);
-    setFullName(event.target.value);
+  const handleFormChange = (event) => {
+    let fieldName = event.target.name;
+    let newValue = event.target.value;
+
+    setFormData((currData) => {
+      return { ...currData, [fieldName]: newValue };
+    });
   };
+
+  const formSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    setFormData({
+      fullName: "",
+      username: "",
+      password: "",
+    });
+  };
+
   return (
-    <form>
-      <label htmlFor="username">Full Name</label>
+    <form onSubmit={formSubmit}>
+      <label htmlFor="fullName">Full Name</label>
+      <input
+        type="text"
+        id="fullName"
+        placeholder="Enter full name"
+        value={formData.fullName}
+        name="fullName"
+        onChange={handleFormChange}
+      />
+      <br />
+      <br />
+      <label htmlFor="username">UserName</label>
       <input
         type="text"
         id="username"
-        placeholder="Enter full name"
-        value={fullName}
-        onChange={handleChangeName}
+        placeholder="Enter username"
+        value={formData.username}
+        name="username"
+        onChange={handleFormChange}
       />
+      <br />
+      <br />
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        id="password"
+        placeholder="Enter Password"
+        value={formData.password}
+        name="password"
+        onChange={handleFormChange}
+      />
+      <br />
+      <br />
       <button>Submit</button>
     </form>
   );
